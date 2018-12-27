4d6d1c38bdf46cacf0851c186e64f2feb9200a5d
Hoang Vo <hoangvo@gmail.com>
2018-12-27 13:16:49 -0600

    feature: consumer demo with threading

---
+++ src/main/java/com/voco/kafka/tutorial1/ConsumerDemoWithThread.java
@@ -0,0 +1,112 @@
+package com.voco.kafka.tutorial1;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class ConsumerDemoWithThread {
+    public static void main(String[] args) {
+        new ConsumerDemoWithThread().run();
+    }
+
+    public class ConsumerRunnable implements Runnable {
+
+        private CountDownLatch latch;
+        private KafkaConsumer<String, String> consumer;
+        private Logger logger = LoggerFactory.getLogger(ConsumerRunnable.class.getName());
+
+        public ConsumerRunnable(String bootstrapServers,
+                              String groupId,
+                              String topic,
+                              CountDownLatch latch) {
+            this.latch = latch;
+
+            // 1. create consumer configs
+            Properties properties = new Properties();
+            properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+            properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+            properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+            // 2. create consumer
+            consumer = new KafkaConsumer<>(properties);
+
+            // 3. subscribe consumer to our topic(s)
+            consumer.subscribe(Arrays.asList(topic));
+        }
+
+        @Override
+        public void run() {
+            // 4. poll for new data
+            try {
+                while (true) {  // purely for demo purpose
+                    ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                    for (ConsumerRecord<String, String> record : records) {
+                        logger.info("Key: " + record.key() + ", Value: " + record.value());
+                        logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+                    }
+                }
+            } catch (WakeupException wue) {
+                logger.info("Received shutdown signal!");
+            } finally {
+                consumer.close();   // very important to close consumer!
+                latch.countDown();  // tells our main code we're done w/ the consumer
+            }
+        }
+
+        public void shutdown() {
+            // the wakeup() method is a special method to interrupt consumer.poll()
+            // it will throw the exception, WakeUpException
+            consumer.wakeup();
+        }
+    }
+
+    private ConsumerDemoWithThread() {}
+
+    private void run() {
+        Logger logger = LoggerFactory.getLogger(ConsumerDemoWithThread.class.getName());
+
+        String bootstrapServers = "localhost:9092";
+        String groupId = "my-sixth-app";
+        String topic = "first_topic";
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        logger.info("Creating the consumer thread");
+        Runnable myConsumerRunnable = new ConsumerRunnable(bootstrapServers, groupId, topic, countDownLatch);
+
+        // start thread
+        Thread myThread = new Thread(myConsumerRunnable);
+        myThread.start();
+
+        // add a shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Caught shutdown hook");
+            ((ConsumerRunnable) myConsumerRunnable).shutdown();
+            try {
+                countDownLatch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            logger.info("Application has exited");
+        }));
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException ie) {
+            logger.error("Application got interrupted", ie);
+        } finally {
+            logger.info("Application is closing");
+        }
+    }
+}
