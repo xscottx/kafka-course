60d591cac816379b99358660dd653ae94c8ae596
Hoang Vo <hoangvo@gmail.com>
2018-12-28 08:53:29 -0600

    feature: set up the safe producer

--- src/main/java/com/voco/kafka/tutorial2/TwitterProducer.java
+++ src/main/java/com/voco/kafka/tutorial2/TwitterProducer.java
@@ -113,8 +113,16 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // 2a. create a safe producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");  // kafka 2.0 >= 1.1
+
         // 2. create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+
         return producer;
     }
 }
