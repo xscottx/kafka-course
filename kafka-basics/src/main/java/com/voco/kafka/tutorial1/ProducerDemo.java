32175e7bc39b4558ebc78b0931d9b2609b657955
Hoang Vo <hoangvo@gmail.com>
2018-12-28 09:32:36 -0600

    chore: refactor into multi modules

--- kafka-basics/src/main/java/com/voco/kafka/tutorial1/ProducerDemo.java
+++ kafka-basics/src/main/java/com/voco/kafka/tutorial1/ProducerDemo.java
@@ -1,6 +1,8 @@
 package com.voco.kafka.tutorial1;
 
-import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
